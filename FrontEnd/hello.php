5e4870fb0cdc1be065b34823ed2b31ccad01e6dd
MichaelMa <Minima2014@iCloud.com>
2016-11-07 15:19:20 +0800

    Follow manual and Add hello.php

---
+++ FrontEnd/hello.php
@@ -0,0 +1,15 @@
+/**
+ * Created by PhpStorm.
+ * User: MichaelMa
+ * Date: 11/7/16
+ * Time: 15:17
+ */
+
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?>
+</body>
+</html>
