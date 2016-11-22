91d591f2cde90281318277c4b2efcced3090bf97
auas <jinshenandy@163.com>
2016-11-22 18:51:46 +0800

    outline
    finish logging in and signing up and checking info for student.

--- BackEnd/database.py
+++ BackEnd/database.py
@@ -1,5 +1,6 @@
 
 import pymysql
+import random
 # check data base~ OK
 conn = pymysql.connect(host='127.0.0.1', port=6666, user='root', passwd='666666',db='overseasdb')
 cur = conn.cursor()
@@ -12,6 +13,9 @@ rest = cur.fetchall()
 for r in rest:
             print(r)
 '''
+
+
+
 #rest=cur.fetchall()
 def get_id(table,col):
     s = "select max({0}) from {1}".format(col,table)
@@ -31,7 +35,14 @@ def chq_score(name,score):
 def search(table,col,condition):
     s = "select {0} from {1} where {2}".format(col,table,condition)
     cur.execute(s)
-    return cur.fetchall()
+    ret = cur.fetchall()
+    for it in ret:
+        print(it)
+    return ret
+def isThere(table,condition):
+    rest = search(table, "*", condition)
+    #print(rest==())
+    return not (rest==())
 def update_professor(name,homepage,major,UnName):
     cur.execute("select count(*) from professor")
     res = cur.fetchall()
@@ -55,6 +66,7 @@ def show(table):
     rest = cur.fetchall()
     for r in rest:
         print(r)
+    #return rest
 def update_itm(table,col,val,condition):
     t = 1
     if type(val)==type(t):
@@ -63,22 +75,34 @@ def update_itm(table,col,val,condition):
         s1 = "update {0} set {1}='{2}' where {3}".format(table, col, val, condition)
     cur.execute(s1)
     conn.commit()
-def add_user(who_,name,password,email):#professor give id ;school give school name
+def add_user(who_,name,password,email,uni):#professor give id ;school give school name
+    print("-3")
     id = get_id("user","userId")
+    print("-2")
     rr = search("user","*","userName = '{0}'".format(name))
+    print("-1")
     if rr ==():
-        #print("adding user!")
-        s = "insert into user(userId,userName,userPassword,email)values({0},'{1}','{2}','{3}')".format(id,name,password,email)
+
+        print("adding user!")
+        s = "insert into user(userId,userName,userPassword,email,uni)values({0},'{1}','{2}','{3}','{4}')".format(id,name,password,email,uni)
         cur.execute(s)
         conn.commit()
+        print("in 1")
     else:
         print("userName used! try another one~")
-        return
+        return '-2' #double name
     if who_=="school":
+        print("in 2")
         update_itm("universityus","userId",id,"uniUName = '{0}'".format(name))
+        print("in 3")
     else:
-        update_itm("professor","userId",id,"professorId = {0}".format(who_))
+        check = isThere("professor","email = '{0}'".format(email))
+        print("in 4")
+        if ~check:
+            return '-3'
+        update_itm("professor","userId",id,"email = '{0}'".format(email))
     conn.commit()
+    return '0'
 
 def add_new_itm(table,col,id):
     s = "insert into {0}({1})values({2})".format(table,col,id)
@@ -93,33 +117,122 @@ def delet(table,condition):
 def delet_graduate(id):
     delet("graduate","graduateId = {0}".format(id))
 
-def add_std_user(name,password,gender):
+def set_TF(reading,speaking,listening,writing):
+    i
+
+def add_std_user(name,password,gender,tel,sname,stdSchool):
     rr = search("studentuser","*","SUserName = '{0}'".format(name))
     if rr ==():
         id = get_id("studentuser","SUserId")
         #init grade
         gradeId = get_id("grade","gradeId")
         add_new_itm("grade", "gradeId", gradeId)
-        s = "insert into studentuser(SUserId,SUserName,SUserPassword,SUserGender,gradeId)" \
-            "values({0},'{1}','{2}','{3}',{4})".format(id, name,password,gender,gradeId)
+        args = (gradeId,)
+        cur.callproc("init_tf_by_gradeId", args)
+        # add_new_itm("grade", "GREQuantitative", 0)
+        # add_new_itm("grade", "GREVerbal", 0)
+        # add_new_itm("grade", "GREAnaliticalWriting", 0)
+        # add_new_itm("grade", "TOEFLListening", 0)
+        # add_new_itm("grade", "TOEFLReading", 0)
+        # add_new_itm("grade", "TOEFLWriting", 0)
+        # add_new_itm("grade", "TOEFLSpeaking", 0)
+        # add_new_itm("grade", "IELTSListening", 0)
+        # add_new_itm("grade", "IELTSReading", 0)
+        # add_new_itm("grade", "IELTSWriting", 0)
+
+        s = "insert into studentuser(SUserId,SUserName,SName,SUserPassword,SUserGender,gradeId,tel,idealUni)" \
+            "values({0},'{1}','{2}','{3}','{4}',{5},'{6}','{7}')".format(id, name,sname,password,gender,gradeId,tel,stdSchool)
         cur.execute(s)
         conn.commit()
+        return '0' # finish successfully
     else:
         print("user name occupied! try another one!")
+        return '-1' # double named!
 
-add_std_user("jinshenandy","jinshenandy","male")
+def std_log_in(name,password):
+    q = isThere("studentuser","SUserName='{0}' and SUserPassword = '{1}'".format(name,password))
+    if q:
+        return '0'
+    else:
+        return '-1'
 
-args = (2,)
-cur.callproc("get_grade_by_id",args)
-ret = cur.fetchall()
-for it in ret:
-    print(it)
+def pu_log_in(name, password):
+    q = isThere("user", "userName='{0}' and userPassword = '{1}'".format(name, password))
+    if q:
+        return '0'
+    else:
+        return '-1'
 
-show("graduate")
-show("general_info")
-delet_graduate(5)
-show("general_info")
-show("graduate")
+def get_stduser_info(name):
+    args = (name,)
+    cur.callproc("get_stduser_info_by_name", args)
+    ret = cur.fetchall()
+    r1 = list(ret[0])
+    cur.callproc("get_stduser_grade_by_name", args)
+    ret = cur.fetchall()
+    r2 = list(ret[0])
+    r = r1+r2[1:8]
+    return r
+
+
+def update_std_grade(name,r,l,s,w,v,q,aw):
+    args1=(name,s,r,w,l)
+    args2=(name,q,v,aw)
+    cur.callproc("std_TF_grade", args1)
+    cur.callproc("std_GRE_grade", args2)
+    conn.commit()
+
+def update_std_password(name,old_p,new_p):
+    chq = std_log_in(name,old_p)
+    if chq == '-1':
+        return chq
+    else:
+        update_itm("studentuser","SUserPassword",new_p,"SUserName='{0}'".format(name))
+        return '0'
+
+def UniSchChq(name):
+    args=(name,)
+    cur.callproc("get_uni_sch_by_name", args)
+    rest = cur.fetchall()
+    print("%%%%%%")
+    for it in rest:
+        print(it)
+
+
+    return rest
+
+
+def findIdealSch(name):
+    s = "select idealUni from studentuser where SUserName = '{0}'".format(name)
+    cur.execute(s)
+    ret = cur.fetchall()
+    print("^^^^^^^^^^^")
+    print(ret[0])
+    return ret[0]
+
+
+findIdealSch("")
+#UniSchChq("Princeton University")
+
+#get_stduser_info("Minima")
+# def check_pass(idnt,name,password):
+#     if idnt =="student":
+#     elif: idnt =="professor"
+#     elif:
+
+#update_graduate("kkk","male",)
+# args = (2,)
+# cur.callproc("get_grade_by_id",args)
+# ret = cur.fetchall()
+# for it in ret:
+#     print(it)
+#
+# show("graduate")
+# show("general_info")
+# delet_graduate(5)
+# show("general_info")
+# show("graduate")
+#add_std_user("jinshenandy","jinshenandy","male")
 
 #delet("user","userId = 1")
 
@@ -138,3 +251,12 @@ major = "computer science"
 UnName = "Harvard University"
 update_professor(name,homepage,major,UnName)
 '''
+
+def get_randbool():
+    rest = random.uniform(10, 20)
+    if rest>15:
+        return '0'
+    else:
+        return '1'
+
+
--- BackEnd/testFlask.py
+++ BackEnd/testFlask.py
@@ -1,46 +1,305 @@
 
-
-'''
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run()
-
-
-'''
-
 from flask import request
 from flask import Flask
+import time
+import datetime
+from datetime import timedelta
 import json
-app = Flask(__name__)
-@app.route('/')
-def h():
-    return 'hhh'
+from flask import Flask
+from flask import request, make_response, current_app
+from functools import update_wrapper
+import json
+import database as db
 
-@app.route('/signUpUser', methods=['POST'])
-def login():
+import globVar as g
+app = Flask(__name__)
+
+basestring = (str, bytes)
+
+# TFscore = []
+# GREscore = []
+# stdUserName = " "
+# stdSchool = " "
+# stdName = " "
+
+#db.add_user(1,1,1,1,1)
+
+#print(db.isThere("studentuser","SUserName = 'Minima123' and SUserPassword = '1234'"))
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+
+    return decorator
+
+
+@app.route('/stSignUp', methods=['POST']) #std user signUp
+@crossdomain(origin='*')
+def stSignUp():
+    #return '0'
+    print("1")
     if request.method == 'POST':
-        TFscore = []
+        print("2")
+        name = request.form.get('new-username')
+        g.stdUserName = name
+        print(name)
+        repeat_password = request.form.get('new-password')
+        print(repeat_password)
+        password = request.form.get('new-repeat-password')
+        print(password)
+        gender = request.form.get('new-gender')
+        print(gender)
+        tel = request.form.get('new-tel')
+        print(tel)
+        if repeat_password == password:
+            ret = db.add_std_user(name,password,gender,tel,g.stdName,g.stdSchool)
+            '''
+            print("***&&&******")
+            for it in TFscore:
+                print(it)
+            for it in GREscore:
+                print(it)
+            print("***&&&******")
+            '''
+
+            db.update_std_grade(name, g.TFscore[0], g.TFscore[1], g.TFscore[2], g.TFscore[3], g.GREscore[0], g.GREscore[1],g.GREscore[2])
+        else:
+            ret = '-2' # pass not same
+        return ret #
+
+@app.route('/puSignUp', methods=['POST'])
+@crossdomain(origin='*')
+def puSignUp():
+    tmp = request.form['new-role']
+    if tmp[0]=='P':
+        role = "professor"
+    else:
+        role = "school"
+    print(role)
+    uni = request.form['new-uni']
+    print(uni)
+    password = request.form['new-password']
+    print(password)
+    rpt_password = password
+    print(rpt_password)
+    email = request.form['new-email']
+    print(email)
+    name = request.form['new-username']
+
+    print(name)
+    if password==rpt_password:
+
+        ret = db.add_user(role, name, password, email, uni) #(who_,name,password,email,uni)
+    else:
+        ret = '-1'
+    return ret
+
+
+@app.route('/stdFront', methods=['POST'])
+@crossdomain(origin='*')
+def stdFront():
+    #return "0"
+    if request.method == 'POST':
+
         a = request.form['TOEFLR']
         if a:
+            TFscore=[]
             TFscore.append(a)
             TFscore.append(request.form['TOEFLL'])
             TFscore.append(request.form['TOEFLS'])
             TFscore.append(request.form['TOEFLW'])
-        for it in a:
-            print(a)
-        return 'Ok'
+        for it in TFscore:
+            print(it)
+
+        b = request.form['GREVerbal']
+        if b:
+            GREscore=[]
+            GREscore.append(b)
+            GREscore.append(request.form['GREQuantitative'])
+            GREscore.append(request.form['GREAW'])
+        for it in GREscore:
+            print(it)
+        #stdUserName = request.form['username']
+        g.GREscore = GREscore
+        g.TFscore = TFscore
+        g.show_grade()
+        g.stdName = request.form['name']
+        g.stdSchool = request.form['uni']
+        print(g.stdUserName)
+        print(g.stdSchool)
+       # return "0"
     else:
-        return "finish !~"
+        i=0
+    return db.get_randbool()
     # the code below this is executed if the request method
     # was GET or the credentials were invalid
 
+@app.route('/stdLogin', methods=['POST']) #std user signUp
+@crossdomain(origin='*')
+def stdLogIn():
+    name = request.form['username']
+    password = request.form['password']
+    ret = db.std_log_in(name,password)
+    print(name)
+    print(type(name))
+    print(password)
+    print(ret)
+    return ret
+
+
+
+@app.route('/puLogin', methods=['POST']) #std user signUp
+@crossdomain(origin='*')
+def puLogin():
+    name = request.form['username']
+    password = request.form['password']
+    ret = db.pu_log_in(name,password)
+    print(name)
+    print(type(name))
+    print(password)
+    print(ret)
+    return ret
+
+
+@app.route('/stdCheck', methods=['GET'])
+@crossdomain(origin='*')
+def stdCheck():
+    name = request.args.get('username')
+    print(name)
+    rest = db.get_stduser_info(name)
+    print("**********")
+    print(type(rest))
+    print(rest)
+    dic = {}
+    dic["gender"]=rest[0]
+    dic["tel"]=rest[1]
+    dic["GREQuantitative"]=rest[2]
+    dic["GREVerbal"]=rest[3]
+    dic["GREAW"]=rest[4]
+    dic["TOEFLL"]=rest[5]
+    dic["TOEFLR"]=rest[6]
+    dic["TOEFLW"] = rest[7]
+    dic["TOEFLS"] = rest[8]
+    print(dic)
+    return json.dumps(dic)
+
+
+@app.route('/stdUpgrade', methods=['POST'])
+@crossdomain(origin='*')
+def stdUpgrade():
+    print("***&&&")
+    name = request.form['username']
+    print(name)
+    b = request.form['GREVerbal']
+    if b:
+        GREscore = []
+        GREscore.append(b)
+        GREscore.append(request.form['GREQuantitative'])
+        GREscore.append(request.form['GREAW'])
+
+
+    a = request.form['TOEFLR']
+    if a:
+        TFscore = []
+        TFscore.append(a)
+        TFscore.append(request.form['TOEFLL'])
+        TFscore.append(request.form['TOEFLS'])
+        TFscore.append(request.form['TOEFLW'])
+    # for it in TFscore:
+    #     print(it)
+    # for it in GREscore:
+    #     print(it)
+    g.GREscore = GREscore
+    g.TFscore = TFscore
+    g.show_grade()
+    db.update_std_grade(name,g.TFscore[0],g.TFscore[1],g.TFscore[2],g.TFscore[3],g.GREscore[0],g.GREscore[1],g.GREscore[2])
+    return '0'
+
+@app.route('/stdUpPass', methods=['POST'])
+@crossdomain(origin='*')
+def stdUpPass():
+    name = request.form['username']
+    op = request.form['old-password']
+    new_op = request.form['new-password']
+    new_rpt_op = request.form['new-repeat-password']
+    if new_op==new_rpt_op:
+        ret = db.update_std_password(name,op,new_op)
+    else:
+        ret = '-2'
+    return ret
+
+
+@app.route('/stdCqUni', methods=['POST'])
+@crossdomain(origin='*')
+def stdCqUni():
+    name = request.form['username']
+    uniName = db.findIdealSch(name)
+    rest = db.UniSchChq(uniName)
+    r1 = rest[0]
+    r2 = rest[1]
+    r3 = rest[2]
+    dic={}
+    #dic["gender"] = rest[0]
+    dic["homepage"]=r1[1]
+    dic["rank"]=r1[2]
+    dic["salary"]=r1[3]
+
+    dic["school1"]=r1[4]
+    dic["school-rank1"]=r1[5]
+    dic["school-salary1"]=r1[6]
+
+    dic["school2"] =r2[4]
+    dic["school-rank2"] =r2[5]
+    dic["school-salary2"] =r2[6]
+
+    dic["school3"] =r3[4]
+    dic["school-rank3"] =r3[5]
+    dic["school-salary3"] =r3[6]
+    return json.dumps(dic)
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
 
